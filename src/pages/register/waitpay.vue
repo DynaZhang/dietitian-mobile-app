d5eec2a3c23350761f16a6370cf44f2949c1a2d1
DynaZhang <13339687922@163.com>
2024-03-02 13:06:58 +0800

    upload page

---
+++ src/pages/register/waitpay.vue
@@ -0,0 +1,312 @@
+<template>
+  <view class="wait-pay-page">
+    <view class="remain-time-wrapper">
+      <view class="desc-text">支付剩余时间</view>
+      <view class="remain-time">14:26:00</view>
+    </view>
+    <view class="content-box">
+      <view class="title-wrapper">
+        <text class="title-text">订单信息</text>
+      </view>
+      <view class="list-wrapper">
+        <view class="row">
+          <text class="row-label">服务内容：</text>
+          <text class="row-value">健康管理师资格证（三级）</text>
+        </view>
+        <view class="row">
+          <text class="row-label">收款主体：</text>
+          <text class="row-value">北京知诚教育有限责任公司</text>
+        </view>
+        <view class="row">
+          <text class="row-label">订单编号：</text>
+          <text class="row-value">786786856567565</text>
+        </view>
+        <view class="row">
+          <text class="row-label">总计价格：</text>
+          <text class="row-value price">￥5000</text>
+        </view>
+      </view>
+    </view>
+    <view class="need-pay-wrapper">
+      <text class="desc">需支付</text>
+      <text class="value">¥3999</text>
+    </view>
+    <view class="footer-wrapper">
+      <view class="cart-wrapper">
+        <view class="price-text">
+          <text class="fact-value">¥3999</text>
+          <text class="pre-value">¥5000</text>
+        </view>
+        <view class="btn-group" @click="handleRegister">
+          <view class="cancel-btn">取消报名</view>
+          <view class="regist-btn">立即报名</view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+const remainTime = ref<number>(0);
+</script>
+
+<style lang="scss" scoped>
+.wait-pay-page {
+  .remain-time-wrapper {
+    background-color: #fff;
+    height: 180rpx;
+    padding: 32rpx;
+    box-sizing: border-box;
+    .desc-text {
+      margin-bottom: 12rpx;
+      font-weight: 400;
+      font-size: 24rpx;
+      color: #262626;
+      text-align: center;
+    }
+    .remain-time {
+      font-weight: 500;
+      font-size: 60rpx;
+      color: #43b9a6;
+      text-align: center;
+    }
+  }
+  .content-box {
+    margin: 30rpx 0;
+    padding: 32rpx;
+    box-sizing: border-box;
+    .title-wrapper {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin-bottom: 30rpx;
+      background-color: #fff;
+      background-image: url("../../static/icons/title.png");
+      background-size: 50rpx 40rpx;
+      background-repeat: no-repeat;
+      overflow-y: hidden;
+      .title-text {
+        margin-top: 7rpx;
+        margin-left: 14rpx;
+        font-weight: bolder;
+        font-size: 32rpx;
+        color: #242833;
+        line-height: 45rpx;
+        letter-spacing: 2rpx;
+      }
+      .introduce-wrapper {
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        .question-icon {
+          width: 32rpx;
+          height: 32rpx;
+          margin-right: 3rpx;
+        }
+        .introduce-text {
+          font-size: 28rpx;
+          color: #949699;
+          line-height: 40rpx;
+          letter-spacing: 1px;
+        }
+        .link-text {
+          display: flex;
+          align-items: center;
+          font-weight: 400;
+          font-size: 28rpx;
+          color: #43b9a6;
+          line-height: 28rpx;
+          cursor: pointer;
+        }
+      }
+    }
+    .card-wrapper {
+      display: flex;
+      justify-content: space-between;
+      .card-item {
+        position: relative;
+        width: 334rpx;
+        height: 200rpx;
+        background-size: contain;
+        background-repeat: no-repeat;
+        padding: 37rpx 32rpx;
+        box-sizing: border-box;
+        &.green {
+          margin-right: 25rpx;
+          background-image: url("../../static/icons/greenBg.png");
+          &.selected {
+            background-image: url("../../static/icons/greenSelectedBg.png");
+            &::after {
+              content: "";
+              position: absolute;
+              top: 0;
+              right: 0;
+              width: 86rpx;
+              height: 74rpx;
+              background-image: url("../../static/icons/greenBadge.png");
+              background-size: contain;
+              background-repeat: no-repeat;
+            }
+          }
+        }
+        &.blue {
+          background-image: url("../../static/icons/blueBg.png");
+          &.selected {
+            background-image: url("../../static/icons/blueSelectedBg.png");
+            &::after {
+              content: "";
+              position: absolute;
+              top: 0;
+              right: 0;
+              width: 86rpx;
+              height: 74rpx;
+              background-image: url("../../static/icons/blueBadge.png");
+              background-size: contain;
+              background-repeat: no-repeat;
+            }
+          }
+        }
+        .card-title {
+          display: block;
+          font-weight: 600;
+          font-size: 32rpx;
+          color: #ffffff;
+          line-height: 32rpx;
+          text-shadow: 0rpx 2rpx 13rpx rgba(0, 0, 0, 0.17);
+        }
+        .price {
+          display: block;
+          margin-top: 26rpx;
+          .per {
+            font-weight: 400;
+            font-size: 24rpx;
+            color: #ffffff;
+            line-height: 24rpx;
+            text-shadow: 0rpx 2rpx 13rpx rgba(0, 0, 0, 0.17);
+          }
+          .value {
+            font-weight: 600;
+            font-size: 34rpx;
+            color: #ffffff;
+            line-height: 34rpx;
+            text-shadow: 0rpx 2rpx 13rpx rgba(0, 0, 0, 0.17);
+          }
+        }
+        .card-desc {
+          display: block;
+          margin-top: 18rpx;
+          font-weight: 400;
+          font-size: 26rpx;
+          color: #ffffff;
+          line-height: 26rpx;
+          text-shadow: 0rpx 2rpx 13rpx rgba(0, 0, 0, 0.17);
+        }
+      }
+    }
+    .list-wrapper {
+      .row {
+        display: flex;
+        margin-bottom: 20rpx;
+        &:last-child {
+          margin-bottom: 0;
+        }
+        &-label {
+          display: inline-block;
+          width: 160rpx;
+          font-weight: 400;
+          font-size: 28rpx;
+          color: #999999;
+          line-height: 28rpx;
+        }
+        &-value {
+          font-weight: 400;
+          font-size: 28rpx;
+          color: #242833;
+          line-height: 28rpx;
+          &.price {
+            color: #fe6710;
+          }
+        }
+      }
+    }
+  }
+  .need-pay-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 28rpx 32rpx;
+    box-sizing: border-box;
+    .desc {
+      font-weight: 400;
+      font-size: 28rpx;
+      color: #999999;
+    }
+    .value {
+      font-weight: 500;
+      font-size: 28rpx;
+      color: #fe6710;
+    }
+  }
+  .footer-wrapper {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 124rpx;
+    z-index: 9999;
+    .cart-wrapper {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 20rpx 40rpx;
+      box-sizing: border-box;
+      background-color: #ffffff;
+      .price-text {
+        .fact-value {
+          margin-right: 24rpx;
+          font-weight: 500;
+          font-size: 34rpx;
+          color: #42b8a5;
+        }
+        .pre-value {
+          font-weight: 400;
+          font-size: 26rpx;
+          color: #8c919d;
+          text-decoration: line-through;
+        }
+      }
+      .btn-group {
+        display: flex;
+        align-items: center;
+        height: 90rpx;
+        cursor: pointer;
+        .cancel-btn {
+            padding: 30rpx;
+            box-sizing: border-box;
+            height: 100%;
+          background: #e8fdf4;
+          font-weight: 500;
+          font-size: 32rpx;
+          color: #4cd0ba;
+          line-height: 30rpx;
+          text-align: center;
+          border-radius: 50rpx 0rpx 0rpx 50rpx;
+        }
+        .regist-btn {
+            padding: 30rpx;
+            box-sizing: border-box;
+            height: 100%;
+          background: #4cd0ba;
+          font-weight: 500;
+          font-size: 32rpx;
+          color: #ffffff;
+          line-height: 30rpx;
+          text-align: center;
+          border-radius: 0px 50rpx 50rpx 0px;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/pages/upload/index.vue
+++ src/pages/upload/index.vue
@@ -1,11 +1,180 @@
-<template></template>
+<template>
+  <view class="upload-page">
+    <view class="content-box">
+      <view class="title-wrapper">
+        <text class="title-text">订单信息</text>
+      </view>
+      <view class="card-wrapper">
+        <view class="card-top">
+          <view class="info-wrapper">
+            <view class="user-avatar">
+              <cover-image :src="iconMap.upload" />
+            </view>
+            <view class="user-info">
+              <view class="main-text">报考指导老师</view>
+              <view class="desc-text">2023-04-13 发布</view>
+            </view>
+          </view>
+          <view class="status-tag uploading">进行中</view>
+        </view>
+        <view class="card-content">
+          <view class="main-text">请上传详细报名信息</view>
+          <view class="desc-text">请上传免冠照等</view>
+        </view>
+        <view class="card-bottom">
+          <view class="upload-status">
+            <view class="status-icon"
+              ><cover-image :src="iconMap.upload"
+            /></view>
+            <text class="main-text">信息已上传 核验中</text>
+          </view>
+          <view class="desc-text">信息已上传</view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
+import uploadIcon from "../../static/icons/upload.png";
+import uploadingIcon from "../../static/icons/uploading.png";
+import uploadSuccessIcon from "../../static/icons/uploadSuccess.png";
+import uploadFailIcon from "../../static/icons/uploadFail.png";
 
-export default defineComponent({
-  setup() {},
+const iconMap = ref({
+  upload: uploadIcon,
+  uploading: uploadingIcon,
+  success: uploadSuccessIcon,
+  fail: uploadFailIcon,
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.upload-page {
+  padding: 30rpx 20rpx;
+  .content-box {
+    box-sizing: border-box;
+    .title-wrapper {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin-bottom: 30rpx;
+      background-color: #fff;
+      background-image: url("../../static/icons/title.png");
+      background-size: 50rpx 40rpx;
+      background-repeat: no-repeat;
+      overflow-y: hidden;
+      .title-text {
+        margin-top: 7rpx;
+        margin-left: 14rpx;
+        font-weight: bolder;
+        font-size: 32rpx;
+        color: #242833;
+        line-height: 45rpx;
+        letter-spacing: 2rpx;
+      }
+    }
+    .card-wrapper {
+      margin-top: 30rpx;
+      padding: 40rpx;
+      box-sizing: border-box;
+      background: #ffffff;
+      box-shadow: 0rpx 10rpx 40rpx -10rpx rgba(30, 37, 49, 0.1);
+      border-radius: 20rpx;
+      border: 1rpx solid rgba(56, 56, 56, 0.07);
+      .card-top {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .info-wrapper {
+          display: flex;
+          align-items: center;
+          .user-avatar {
+            margin-right: 20rpx;
+            width: 60rpx;
+            height: 60rpx;
+            border-radius: 100%;
+          }
+          .user-info {
+            .main-text {
+              font-weight: 400;
+              font-size: 28rpx;
+              color: #242833;
+              line-height: 28rpx;
+            }
+            .desc-text {
+              font-weight: 400;
+              font-size: 24rpx;
+              color: #949699;
+              line-height: 28rpx;
+            }
+          }
+        }
+        .status-tag {
+          width: 90rpx;
+          height: 32rpx;
+          border-radius: 6rpx;
+          font-weight: 400;
+          font-size: 22rpx;
+          line-height: 30rpx;
+          text-align: center;
+          &.uploading {
+            color: #4ac2bf;
+            border: 1rpx solid #4ac2bf;
+          }
+          &.ended {
+            color: #b6b8bf;
+            border: 1rpx solid #b6b8bf;
+          }
+        }
+      }
+      .card-content {
+        margin-top: 32rpx;
+        .main-text {
+          font-size: 32rpx;
+          color: #242833;
+          letter-spacing: 1rpx;
+        }
+        .desc-text {
+          font-size: 28rpx;
+          color: #949699;
+          letter-spacing: 1rpx;
+        }
+      }
+      .card-bottom {
+        margin-top: 32rpx;
+        padding: 30rpx;
+        box-sizing: border-box;
+        background: #fdfdfd;
+        border-radius: 8rpx;
+        border: 1rpx solid #e8e8eb;
+        .upload-status {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border-bottom: 1rpx solid #e3e3e8;
+          padding-bottom: 26rpx;
+          .status-icon {
+            width: 34rpx;
+            height: 34rpx;
+            margin-right: 20rpx;
+          }
+          .main-text {
+            font-weight: 400;
+            font-size: 28rpx;
+            color: #242833;
+          }
+        }
+        .desc-text {
+          margin-top: 20rpx;
+          font-weight: 400;
+          font-size: 28rpx;
+          color: #949699;
+          text-align: center;
+        }
+      }
+    }
+  }
+}
+</style>
